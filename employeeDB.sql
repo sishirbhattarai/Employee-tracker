bc81ace081e36439ca1892b81e5278011c56f6b7
Sishir <sishir.bhattarai@outlook.com>
2020-12-05 17:01:01 -0700

    added employee_db.sql

---
+++ employeeDB.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departments (
+ id INT NOT NULL AUTO_INCREMENT,
+ name VARCHAR(30) NOT NULL, 
+ PRIMARY KEY (id)
+
+);
+
+CREATE TABLE roles (
+ id INT NOT NULL AUTO_INCREMENT,
+ fir
+ PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee (
+
+);
