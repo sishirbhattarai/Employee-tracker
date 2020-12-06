86bf8bd7f9816bf474de0e0ad26cf6cf573b5f35
Sishir <sishir.bhattarai@outlook.com>
2020-12-05 17:07:28 -0700

    added mysql command on employee DB

--- employeeDB.sql
+++ employeeDB.sql
@@ -13,11 +13,17 @@ CREATE TABLE departments (
 
 CREATE TABLE roles (
  id INT NOT NULL AUTO_INCREMENT,
- fir
+ title VARCHAR(30) NOT NULL,
+ salary DECIMAL,
+ department_id INT NOT NULL,
  PRIMARY KEY (id)
 
 );
 
 CREATE TABLE employee (
-
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(30) NOT NULL,
+ last_name VARCHAR(30) NOT NULL,
+ role_id INT NOT NULL,
+ manager_id INT NOT NULL
 );
